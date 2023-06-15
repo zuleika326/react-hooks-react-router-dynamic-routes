507017744afc98782fbe6f83ddcb8a096ae8ee23
zuleika <njokizuleika@gmail.com>
2023-06-15 11:58:38 +0300

    done nested routes

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,12 +1,28 @@
 import React from "react";
-import { Route } from "react-router-dom";
+// import the custom `useRouteMatch` hook from React Router
+import { Route, useRouteMatch } from "react-router-dom";
 import MoviesList from "./MoviesList";
+// import the MovieShow component
+import MovieShow from "./MovieShow";
 
 function MoviesPage({ movies }) {
+  // useRouteMatch returns a special object with information about
+  // the currently matched route
+  const match = useRouteMatch();
+  console.log(match);
+
   return (
     <div>
       <MoviesList movies={movies} />
+      {/* 
+        we can use the current URL from the `match` object as part of the path;
+        this will generate a url like "/movies/:movieId"
+      */}
+      <Route path={`${match.url}/:movieId`}>
+        <MovieShow  movies={movies}/>
+      </Route>
     </div>
   );
 }
+
 export default MoviesPage;
